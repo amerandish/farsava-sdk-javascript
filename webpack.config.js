6ce9fcb5a299d5ea2b523aa908c3eb9185594cd7
Amin Taghikhani <amin76taghikhani@gmail.com>
2020-09-06 13:29:04 +0430

    config webpack and ts for build release

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,77 @@
 const path = require("path");
 const webpack = require("webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const pkg = require("./package.json");
+const banner =
+    "\n" +
+    pkg.name +
+    " -  " +
+    pkg.version +
+    "\n" +
+    pkg.author +
+    "\n" +
+    pkg.homepage +
+    "\n" +
+    "Under " +
+    pkg.license +
+    " license \n" +
+    "\n";
 module.exports = (_env, args) => {
-    const IS_PROD = args.p;
+    const IS_PROD = !!args.p;
+    const MIN = !!args.m;
 
-    const BUILD_PATH = "release";
+    const BUILD_PATH = "dist";
 
-    const JS_FILE_NAME = "farsava-sdk.min.js";
+    let JS_FILE_NAME = "farsava-sdk.js";
 
     const devtool = IS_PROD ? false : "source-map";
 
-    const plugins = [new webpack.optimize.ModuleConcatenationPlugin()];
+    const plugins = [
+        new webpack.BannerPlugin(banner),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+    ];
 
     // if (IS_PROD) {
-    plugins.push(
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: "../typings/farsava.d.ts",
-                    to: "../release/farsava.d.ts",
-                },
-            ],
-        })
-    );
+    //     plugins.push(
+    //         new CopyWebpackPlugin({
+    //             patterns: [
+    //                 {
+    //                     from: "../typings/farsava-sdk.d.ts",
+    //                     to: "../release/farsava-sdk.d.ts",
+    //                 },
+    //             ],
+    //         })
+    //     );
     // }
+    if (MIN) {
+        JS_FILE_NAME = "farsava-sdk.min.js";
+    }
 
     return {
+        mode: IS_PROD ? "production" : "development",
         context: path.resolve(__dirname, "src"),
         entry: "./farsava.ts",
         plugins,
+        optimization: {
+            minimize: MIN,
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: false,
+                }),
+            ],
+        },
         output: {
             path: path.resolve(__dirname, BUILD_PATH),
             filename: JS_FILE_NAME,
-            library: "FarsavaSDK",
-            libraryTarget: "umd",
+            library: "Farsava",
+            libraryTarget: "umd2",
         },
         resolve: {
             extensions: [".ts", ".js"],
         },
         module: {
             rules: [
-                {
-                    test: require.resolve("./src/farsava.ts"),
-                    use: [
-                        {
-                            loader: "expose-loader",
-                            options: {
-                                exposes: ["Farsava"],
-                            },
-                        },
-                    ],
-                },
                 {
                     test: /\.ts$/,
                     use: "ts-loader",
@@ -62,7 +82,6 @@ module.exports = (_env, args) => {
         stats: {
             colors: true,
         },
-
         devtool,
     };
 };
